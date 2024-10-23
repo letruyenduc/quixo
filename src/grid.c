6258fb5e5e837caf7bbba84959ef32189e924bd4
Kevin-OVI <78649489+Kevin-OVI@users.noreply.github.com>
2024-10-23 11:53:32 +0200

    Structure de grille plus intuitive, assertions pour les fonctions de déplacement, et commentaires

--- src/grid.c
+++ src/grid.c
@@ -1,69 +1,138 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
+#define SHIFT_STATUS_OK 0
+#define SHIFT_STATUS_OTHER_PLAYER 1
+
+/**
+ * Description : Structure qui contient la grille de jeu
+ * Auteur : Kevin Carletto
+ * Champs : width contient la largeur de la grille, height la hauteur, et lines les lignes qui elle-mêmes contiennent les cases.
+ */
 struct Grid
 {
     int width;
     int height;
-    char *content;
+    char **lines;
 } typedef Grid;
 
+/**
+ * Description : Fonction de création d'une grille de jeu
+ * Auteur : Kevin Carletto
+ * Paramètres : width est la largeur de la grille, height la hauteur
+ * Traitement : On commence par créer un tableau qui contiendra les lignes de la grille.
+ * On crée ensuite chaque ligne et on les remplie de cases vides.
+ * Enfin, on crée une structure qui contiendra la taille (largeur et hauteur) et le contenu de la grille.
+ * Retour : On retourne une structure contenant la grille de jeu.
+ */
 Grid *createGrid(int width, int height)
 {
-    char *gridContent = malloc(sizeof(char) * width * height);
-    memset(gridContent, ' ', width * height);
+    char **gridLines = malloc(sizeof(char *) * height);
+    for (int i = 0; i < height; i++)
+    {
+        gridLines[i] = malloc(sizeof(char) * width);
+        memset(gridLines[i], ' ', width);
+    }
+
     Grid *grid = malloc(sizeof(Grid));
     grid->width = width;
     grid->height = height;
-    grid->content = gridContent;
+    grid->lines = gridLines;
 
     return grid;
 }
 
+/**
+ * Description : Permet de déplacer une ligne vers la droite
+ * Auteur : Kevin Carletto
+ * Paramètres : grid est la structure de la grille de jeu, row l'indice de la ligne à déplacer, et player le caractère du joueur qui joue.
+ * Traitement : On vérifie que le cube retiré de la grille (la dernière case de la ligne) est vide ou appartient au joueur, sinon on retourne un code d'erreur.
+ * On parcours ensuite la ligne en partant de la fin en déplaçant le contenu de chaque case vers la suivante.
+ * Enfin, on définit la première case de la ligne avec le caractère du joueur qui joue.
+ * Retour : Le code de statut de réussite (1 = le joueur n'a pas le droit de déplacer ce cube, 0 = Tout s'est bien passé)
+ */
 int shiftRowRight(Grid *grid, int row, char player)
 {
-    int rowPos = row * grid->width;
-    char movingBloc = grid->content[rowPos + grid->width - 1];
-    if (movingBloc != ' ' && movingBloc != player)
+    assert(row >= 0 && row < grid->height);
+
+    char movingCube = grid->lines[row][grid->width - 1];
+    if (movingCube != ' ' && movingCube != player)
     {
-        return 1;
+        return SHIFT_STATUS_OTHER_PLAYER;
     }
 
     for (int i = grid->width - 1; i > 0; i--)
     {
-        grid->content[rowPos + i] = grid->content[rowPos + i - 1];
+        grid->lines[row][i] = grid->lines[row][i - 1];
     }
-    grid->content[rowPos] = player;
-    return 0;
+    grid->lines[row][0] = player;
+
+    return SHIFT_STATUS_OK;
 }
 
+/**
+ * Description : Permet de déplacer une ligne vers la gauche
+ * Auteur : Kevin Carletto
+ * Paramètres : grid est la structure de la grille de jeu, row l'indice de la ligne à déplacer, et player le caractère du joueur qui joue.
+ * Traitement : On vérifie que le cube retiré de la grille (la première case de la ligne) est vide ou appartient au joueur, sinon on retourne un code d'erreur.
+ * On parcours ensuite la ligne en partant du début en déplaçant le contenu de chaque case vers la précédente.
+ * Enfin, on définit la dernière case de la ligne avec le caractère du joueur qui joue.
+ * Retour : Le code de statut de réussite (1 = le joueur n'a pas le droit de déplacer ce cube, 0 = Tout s'est bien passé)
+ */
 int shiftRowLeft(Grid *grid, int row, char player)
 {
-    int rowPos = row * grid->width;
-    char movingBloc = grid->content[rowPos];
-    if (movingBloc != ' ' && movingBloc != player)
+    assert(row >= 0 && row < grid->height);
+
+    char movingCube = grid->lines[row][0];
+    if (movingCube != ' ' && movingCube != player)
     {
-        return 1;
+        return SHIFT_STATUS_OTHER_PLAYER;
     }
 
     for (int i = 1; i < grid->width; i++)
     {
-        grid->content[rowPos + i - 1] = grid->content[rowPos + i];
+        grid->lines[row][i - 1] = grid->lines[row][i];
     }
-    grid->content[rowPos + grid->width - 1] = player;
-    return 0;
+    grid->lines[row][grid->width - 1] = player;
+
+    return SHIFT_STATUS_OK;
 }
 
+
+/**
+ * Description : Permet de libérer la mémoire utilisée par la grille de jeu
+ * Auteur : Kevin Carletto
+ * Paramètres : grid est la structure de la grille de jeu.
+ * Traitement : On libère chaque ligne de la grille, puis le tableau de lignes, et enfin la structure elle-même.
+ * Penser à ne plus utiliser le pointeur après cette opération, de préférence le définir à NULL pour en être certain.
+ */
 void freeGrid(Grid *grid)
 {
-    free(grid->content);
+    for (int i = 0; i < grid->height; i++)
+    {
+        free(grid->lines[i]);
+    }
+    free(grid->lines);
     free(grid);
 }
 
+
+/**
+ * Description : Fonction temporaire qui permet d'afficher la grille de jeu. À remplacer par celle utilisant ncurses
+ * Auteur : Kevin Carletto
+ * Paramètres : grid est la structure de la grille de jeu.
+ * Traitement : On commence par afficher une délimitation au dessus de la grille
+ * On parcours chaque ligne de la grille
+ *     On affiche une délimitation sur la gauche de la ligne
+ *     On affiche chaque case de la ligne
+ *     On affiche une délimitation sur la droite de la ligne
+ * On termine par afficher une délimitation en bas de la grille 
+ */
 void printGrid(Grid *grid)
 {
-    for (int j = 0; j < grid->width + 2; j++)
+    for (int j = -2; j < grid->width; j++)
     {
         printf("-");
     }
@@ -73,7 +142,7 @@ void printGrid(Grid *grid)
         printf("|");
         for (int j = 0; j < grid->width; j++)
         {
-            printf("%c", grid->content[i * grid->width + j]);
+            printf("%c", grid->lines[i][j]);
         }
         printf("|\n");
     }
--- src/grid.h
+++ src/grid.h
@@ -1,8 +1,14 @@
+#ifndef GRID_H
+#define GRID_H
+
+#define SHIFT_STATUS_OK 0
+#define SHIFT_STATUS_OTHER_PLAYER 1
+
 struct Grid
 {
     int width;
     int height;
-    char **content;
+    char **lines;
 } typedef Grid;
 
 Grid *createGrid(int width, int height);
@@ -10,3 +16,5 @@ int shiftRowRight(Grid *grid, int row, char player);
 int shiftRowLeft(Grid *grid, int row, char player);
 void freeGrid(Grid *grid);
 void printGrid(Grid *grid);
+
+#endif // GRID_H
