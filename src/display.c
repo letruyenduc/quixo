1578abec6a63a6d24693e879d52f340e35e1b338
Kevin-OVI <78649489+Kevin-OVI@users.noreply.github.com>
2024-11-19 11:26:29 +0100

    Amélioration affichage grille

--- src/display.c
+++ src/display.c
@@ -9,31 +9,31 @@
 #endif
 #include "grid.h"
 
+#define OFFSET_LINE 5
+#define OFFSET_COL 10
+
 void displayGrid(Grid *grid)
 {
     for (int j = 0; j < grid->width; j++)
     {
-        mvprintw(0, j * 2, "+");
-        mvprintw(0, j * 2 + 1, "-");
+        mvprintw(OFFSET_LINE + 0, OFFSET_COL + j * 2, "+");
+        mvprintw(OFFSET_LINE + 0, OFFSET_COL + j * 2 + 1, "-");
     }
-    mvprintw(0, grid->width * 2, "+");
+    mvprintw(OFFSET_LINE + 0, OFFSET_COL + grid->width * 2, "+");
 
     for (int i = 0; i < grid->height; i++)
     {
-        mvprintw(i * 2 + 1, 0, "|");
+        mvprintw(OFFSET_LINE + i * 2 + 1, OFFSET_COL + 0, "|");
 
         for (int j = 0; j < grid->width; j++)
         {
-            mvprintw(i * 2 + 1, j * 2 + 1, "%c", grid->rows[i][j]);
-            mvprintw(i * 2 + 1, j * 2 + 2, "|");
-        }
+            mvprintw(OFFSET_LINE + i * 2 + 1, OFFSET_COL + j * 2 + 1, "%c", grid->rows[i][j]);
+            mvprintw(OFFSET_LINE + i * 2 + 1, OFFSET_COL + j * 2 + 2, "|");
 
-        mvprintw(i * 2 + 2, 0, "+");
-        for (int j = 0; j < grid->width; j++)
-        {
-            mvprintw(i * 2 + 2, j * 2 + 1, "-");
+            mvprintw(OFFSET_LINE + i * 2 + 2, OFFSET_COL + j * 2, "+");
+            mvprintw(OFFSET_LINE + i * 2 + 2, OFFSET_COL + j * 2 + 1, "-");
         }
-        mvprintw(i * 2 + 2, grid->width * 2, "+");
+        mvprintw(OFFSET_LINE + i * 2 + 2, OFFSET_COL + grid->width * 2, "+");
     }
 
     refresh();
@@ -42,26 +42,20 @@ void displayGrid(Grid *grid)
 void handleInput(int *row, int *column, int *function, char *player)
 {
 
-    mvprintw(11, 0, "Ligne : ");
+    mvprintw(OFFSET_LINE + 12, OFFSET_COL + 0, "Ligne : ");
     refresh();
     scanw("%d", row);
 
-    mvprintw(12, 0, "Colonne : ");
+    mvprintw(OFFSET_LINE + 13, OFFSET_COL + 0, "Colonne : ");
     refresh();
     scanw("%d", column);
 
-    mvprintw(13, 0, "0=shiftRowRight, 1=shiftRowLeft, 2=shiftColumnDown, 3=shiftColumnUp ; Fonction : ");
+    mvprintw(OFFSET_LINE + 14, OFFSET_COL + 0, "0=shiftRowRight, 1=shiftRowLeft, 2=shiftColumnDown, 3=shiftColumnUp ; Fonction : ");
     refresh();
     scanw("%d", function);
 
-    mvprintw(14, 0, "Joueur : ");
+    mvprintw(OFFSET_LINE + 15, OFFSET_COL + 0, "Joueur : ");
     refresh();
     *player = (char)getch();
-
-    // Nettoie les zones d'affichage des inputs
-    mvprintw(11, 0, "                                                  ");
-    mvprintw(12, 0, "                                                  ");
-    mvprintw(13, 0, "                                                  ");
-    mvprintw(14, 0, "                                                  ");
     refresh();
 }
