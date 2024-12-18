29ea422af7d4c8247aee305bc44421aa6f54fc0b
JzOshi <letruyenduc@gmail.com>
2024-12-18 13:13:53 +0100

    ajout de getOffset

--- src/display.h
+++ src/display.h
@@ -1,5 +1,8 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
+int getOffsetLine(Grid *grid);
+int getOffsetCol(Grid *grid);
+int getTextOffsetCol(Grid *grid);
 
 void displayGrid(Grid *grid, char nextPlayer, wchar_t *statusMessage, int row, int column, int selectingFunction);
 void handleInput(Grid *grid, char nextPlayer, wchar_t *statusMessage, int *row, int *column, int *function);
