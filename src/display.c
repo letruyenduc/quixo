25775c432f7bf9743eee01a15e8732b83076e969
YakoShiza <titou06.v@gmail.com>
2025-01-09 21:56:03 +0100

    Correction du commit précédent

--- src/display.c
+++ src/display.c
@@ -402,7 +402,7 @@ void handleInput(Grid *grid, Player *nextPlayer, wchar_t *statusMessage, int *ro
  * - grid : La grille de jeu
  * - winner : Le joueur gagnant
  */
-void displayEndScreen(Grid *grid, Player *winner)
+void displayEndScreen(Grid *grid, Player *winner, Player *colorIndex)
 {
     displayGrid(grid, -1, -1, 0);
     mvprintw(getOffsetLine(grid) + 4, getTextOffsetCol(grid), "%s (%c) gagne !", winner->playerName, winner->playerSymbol, winner-> colorIndex);
--- src/display.h
+++ src/display.h
@@ -7,6 +7,6 @@ int getTextOffsetCol(Grid *grid);
 void displayGrid(Grid *grid, int row, int column, int selectingFunction);
 void displayGridAndStatus(Grid *grid, Player *nextPlayer, wchar_t *statusMessage, int row, int column, int selectingFunction);
 void handleInput(Grid *grid, Player *nextPlayer, wchar_t *statusMessage, int *row, int *column, int *function);
-void displayEndScreen(Grid *grid, Player *winner);
+void displayEndScreen(Grid *grid, Player *winner, Player *colorIndex);
 
 #endif // DISPLAY_H
